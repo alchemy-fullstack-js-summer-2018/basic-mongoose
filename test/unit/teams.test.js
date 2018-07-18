5b0c790b80a209349529d7f5ad91c0a97f3544f0
Antreo Pukay <acrosswalker@yahoo.com>
2018-07-17 22:14:55 -0700

    straightened out mongoose issue

--- test/unit/teams.test.js
+++ test/unit/teams.test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const { assert } = chai;
-const Teams = require('../../lib/models/teams'); 
+const Team = require('../../lib/models/team'); 
 
 const getErrors = (validation, numberExpected) => {
     assert.isDefined(validation);
@@ -8,7 +8,7 @@ const getErrors = (validation, numberExpected) => {
     assert.equal(Object.keys(errors).length, numberExpected);
 };
 
-describe('Teams model', () => {
+describe('Team model', () => {
     it('validates good model', () => {
         const data = {
             name: 'Trailblazers',
@@ -19,11 +19,19 @@ describe('Teams model', () => {
             stadium: 'round',
             roster: 13          
         };
-        const team = new Teams(data);
+        const team = new Team(data);
 
         const json = team.toJSON();
         delete json._id;
         assert.deepEqual(json, data);
         assert.isUndefined(team.validateSync());
+    });
+
+    it('rating is at least 1', () => {
+        const team = new Team({});
+        const errors = getErrors(team.validateSync(), 3);
+        assert.equal(errors.name.kind, 'required');
+        assert.equal(errors['location.state'].kind, 'required');
+        assert.equal(errors.roster.kind, 'required');
     });
 });
