7ab432dc485f8c5430b47498ee0ecaa20b7575ae
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-18 10:54:10 -0700

    Implement get all functionality and test

--- test/e2e/shows.test.js
+++ test/e2e/shows.test.js
@@ -4,22 +4,59 @@ const { dropCollection } = require('./db');
 
 describe('TV Shows API', () => {
     beforeEach(() => dropCollection('shows'));
-    let show;
+    let westworldShow;
+    let utopiaShow;
+    let gameOfThronesShow;
+
+    const westworld = {
+        name: 'Westworld',
+        firstAired: '2016',
+        networkType: 'cable'
+    };
+    const utopia = {
+        name: 'Utopia',
+        firstAired: '2013',
+        networkType: 'broadcast'
+    };
+    const gameOfThrones = {
+        name: 'Game of Thrones',
+        firstAired: '2011',
+        networkType: 'cable'
+    };
 
     beforeEach(() => {
-        const data = {
-            name: 'Westworld',
-            firstAired: '2016',
-            networkType: 'cable'
-        };
-
         return request
             .post('/api/shows')
-            .send(data)
-            .then(({ body }) => show = body);
+            .send(westworld)
+            .then(({ body }) => westworldShow = body);
+    });
+    beforeEach(() => {
+        return request
+            .post('/api/shows')
+            .send(utopia)
+            .then(({ body }) => utopiaShow = body);
+    });
+    beforeEach(() => {
+        return request
+            .post('/api/shows')
+            .send(gameOfThrones)
+            .then(({ body }) => gameOfThronesShow = body);
     });
 
     it('saves a show', () => {
-        assert.isOk(show._id);
+        assert.isOk(westworldShow._id);
+    });
+
+    it('gets all shows', () => {
+        return request
+            .get('/api/shows')
+            .then(({ body }) => {
+                assert.equal(body[0].name, westworldShow.name);
+                assert.equal(body[1].name, utopiaShow.name);
+                assert.equal(body[2].name, gameOfThronesShow.name);
+                assert.equal(body[0].firstAired, westworldShow.firstAired);
+                assert.equal(body[1].firstAired, utopiaShow.firstAired);
+                assert.equal(body[2].firstAired, gameOfThronesShow.firstAired);
+            });
     });
 });
