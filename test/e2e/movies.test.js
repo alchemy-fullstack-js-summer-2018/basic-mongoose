597d7883e24d3eb404b52d2dc66a35efe7812c28
MariahAdams <mariah.adams23@yahoo.com>
2018-07-18 09:38:37 -0700

    setting up e2e testing

---
+++ test/e2e/movies.test.js
@@ -0,0 +1,12 @@
+/* e2e testing */
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./_db');
+
+describe.only('Movies API', () => {
+
+    
+    it('saves a company', () => {
+
+    });
+});
