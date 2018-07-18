51d90a6e0c455b9d9fc180a94b7686b91b8350f5
MariahAdams <mariah.adams23@yahoo.com>
2018-07-18 09:43:26 -0700

    file cleanup

--- test/e2e/movies.test.js
+++ test/e2e/movies.test.js
@@ -5,7 +5,32 @@ const { dropCollection } = require('./_db');
 
 describe.only('Movies API', () => {
 
-    
+    beforeEach(() => dropCollection('movies'));
+
+    let movie;
+
+    beforeEach(() => {
+        const data = {
+            name: 'Spirited Away',
+            director: 'Hayao Miyazaki',
+            composer: 'Joe Hisashi',
+            year: 2001,
+            voices: {
+                chihiro: 'Daveigh Chase',
+                haku: 'Jason Marsden',
+                yubaba: 'Suzanne'
+            },
+            runtime: 'just right',
+            rating: 5,
+            isPixar: true,
+            languages: {
+                1: 'English',
+                2: 'Japanese'
+            },
+            keywords: ['Japan', 'Ghibli', 'Pixar', 'pig']
+        };
+    });
+
     it('saves a company', () => {
 
     });
