d3e002360720a57177cfb29fdd3c41ca4e19e3c3
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-18 09:16:46 -0700

    Connect routes and model and have validation test pass

---
+++ test/unit/shows.test.js
@@ -0,0 +1,34 @@
+const chai = require('chai');
+const { assert } = chai;
+const Show = require('../../lib/models/show');
+
+const getErrors = (validation, number) => {
+    assert.isDefined(validation);
+    const errors = validation.errors;
+    assert.equal(Object.keys(errors).length, number);
+    return errors;
+};
+
+describe('TV Show model', () => {
+    const data = {
+        name: 'Game of Thrones',
+        crew: {
+            creators: 'David Benioff & D.B. Weiss',
+            directors: ['Alan Taylor', 'Michelle MacLaren'],
+            cinematographer: 'Alik Sakharov'
+        },
+        firstAired: '2011',
+        networkType: 'cable',
+        seasons: 7,
+        ended: false,
+        actors: ['Peter Dinklage', 'Lena Headey', 'Rose Leslie']
+    };
+    
+    it('validates good model', () => {
+        const show = new Show(data);
+        const json = show.toJSON();
+        delete json._id;
+        assert.deepEqual(json, data);
+        assert.isUndefined(show.validateSync());
+    });
+});
