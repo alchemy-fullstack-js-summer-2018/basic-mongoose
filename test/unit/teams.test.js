92ef14ee1e9bbb9998963ffcbf78448986050640
Antreo Pukay <acrosswalker@yahoo.com>
2018-07-18 09:21:29 -0700

    roster at most 15 test passing

--- test/unit/teams.test.js
+++ test/unit/teams.test.js
@@ -4,9 +4,8 @@ const Team = require('../../lib/models/team');
 
 const getErrors = (validation, numberExpected) => {
     assert.isDefined(validation);
-    const errors = validation.errors;
-    console.log('*** errors ***', numberExpected);
     assert.equal(Object.keys(errors).length, numberExpected);
+    const errors = validation.errors;
 };
 
 describe('Team model', () => {
@@ -32,14 +31,28 @@ describe('Team model', () => {
         const team = new Team({});
         const validation = team.validateSync();
         assert.isDefined(validation);
-
+        
         const errors = validation.errors;
-        // assert.equal(Object.keys(errors).length, 3);
+        assert.equal(Object.keys(errors).length, 3);
         assert.equal(errors.roster.kind, 'required');
-
+        
         // const errors = getErrors(team.validateSync(), 3);
         // assert.equal(errors.name.kind, 'required');
         // assert.equal(errors['location.state'].kind, 'required');
+    });
+    
+    it('roster is at most 15', () => {
+        const team = new Team({
+            name: 'Team',
+            location: { state: 'OR' },
+            roster: 16
+        });
+
+        const validation = team.validateSync();
+        const errors = validation.errors;
+        assert.equal(Object.keys(errors).length, 1);
+        assert.equal(errors.roster.kind, 'max');
+        // const errors = getErrors(team.validateSync(), 1);
     });
     
 });
