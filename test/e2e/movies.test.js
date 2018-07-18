0cba482b6225fc46874683f4d9dd20f44f6cb4b5
MariahAdams <mariah.adams23@yahoo.com>
2018-07-18 11:54:03 -0700

    add get all, test passing

--- test/e2e/movies.test.js
+++ test/e2e/movies.test.js
@@ -7,46 +7,75 @@ describe.only('Movies API', () => {
 
     beforeEach(() => dropCollection('movies'));
 
-    let movie;
+    let spiritedaway;
+    let laputa;
+
+    const spiritedData = {
+        name: 'Spirited Away',
+        director: 'Hayao Miyazaki',
+        composer: 'Joe Hisashi',
+        year: 2001,
+        voices: {
+            chihiro: 'Daveigh Chase',
+            haku: 'Jason Marsden',
+            yubaba: 'Suzanne'
+        },
+        runtime: 'just right',
+        rating: 5,
+        isPixar: true,
+        languages: {
+            1: 'English',
+            2: 'Japanese'
+        },
+        keywords: ['Japan', 'Ghibli', 'Pixar', 'pig']
+    };
+
+    const laputaData = {
+        name: 'Laputa',
+        director: 'Hayao Miyazaki',
+        composer: 'Joe Hisashi',
+        year: 1986,
+        runtime: 'just right',
+        rating: 5,
+        isPixar: true,
+        languages: {
+            1: 'English',
+            2: 'Japanese'
+        },
+        keywords: ['Japan', 'Ghibli', 'Pixar', 'Castle']
+    };
 
     beforeEach(() => {
-        const data = {
-            name: 'Spirited Away',
-            director: 'Hayao Miyazaki',
-            composer: 'Joe Hisashi',
-            year: 2001,
-            voices: {
-                chihiro: 'Daveigh Chase',
-                haku: 'Jason Marsden',
-                yubaba: 'Suzanne'
-            },
-            runtime: 'just right',
-            rating: 5,
-            isPixar: true,
-            languages: {
-                1: 'English',
-                2: 'Japanese'
-            },
-            keywords: ['Japan', 'Ghibli', 'Pixar', 'pig']
-        };
-
         return request 
             .post('/api/movies')
-            .send(data)
-            .then(({ body }) => movie = body);
+            .send(spiritedData)
+            .then(({ body }) => spiritedaway = body);
+    });
+
+    beforeEach(() => {
+        return request 
+            .post('/api/movies')
+            .send(laputaData)
+            .then(({ body }) => laputa = body);
     });
 
     it('saves a movie', () => {
-        assert.isOk(movie._id);
+        assert.isOk(spiritedaway._id);
     });
 
     it('returns movie by id on GET', () => {
         return request  
-            .get(`/api/movies/${movie._id}`)
+            .get(`/api/movies/${spiritedaway._id}`)
             .then(({ body }) => {
-                assert.deepEqual(body, movie);
+                assert.deepEqual(body, spiritedaway);
             });
     });
 
-    
+    it('returns all movies on GET', () => {
+        return request
+            .get('/api/movies')
+            .then(({ body }) => {
+                assert.deepEqual(body, [spiritedaway, laputa]);
+            });
+    });
 });
