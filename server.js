a6075a1b8c1a67de91effbb797dcfcfc5b45e923
Easton Gorishek <ieaston26@gmail.com>
2018-07-18 11:24:02 -0700

    lab done

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-// require('dotenv').config();
 const { createServer } = require('http');
 
 const connect = require('./lib/connect');
 connect('mongodb://localhost:27017/acl');
+
 const app = require('./lib/app');
 
 const PORT = process.env.PORT || 3000;
+
 const server = createServer(app);
 
 server.listen(PORT, () => {
